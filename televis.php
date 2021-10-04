00a679a562a3931f9b4aff870c131274cdfbccdd
Anto-Nobel <nobel2002@gmail.com>
2021-10-04 22:22:15 +0530

    final v0.0.1b

--- televis.php
+++ televis.php
@@ -1,48 +1,101 @@
-<?php
-
+<?php 
 $servername="localhost"; 
 $dbname="id17181951_esp_data"; 
 $username="id17181951_arunkumar"; 
-$password="135/kAN)wCtZ6fKv"; 
+$password="135/kAN)wCtZ6fKv";  
 
-$api_key="jdhvbgsdjvhsdbcvjyegfdmsdcvh1278ewqndb23dbu"; 
-
-$api=$val1=$val2=""; 
-
-if($_SERVER["REQUEST_METHOD"]=="GET")
-{
-	$api=clean($_GET["api_key"]); 
-	if($api_key==$api)
-	{
-		$val2=clean($_GET["p"]); 
-		$val1=clean($_GET["t"]); 
-		$conn=new mysqli($servername,$username,$password,$dbname); 
-		$sql="INSERT INTO Sensor (value1,value2)  
-				VALUES ('" . $val1 . "','" . $val2 . "')";  
-		if($conn->query($sql) === TRUE)
-		{
-			echo "Success";
-		} 
-		else
-		{
-			echo "Failed" . $sql . "<br>" . $conn->error;
-		} 
-		$conn->close();
-	} 
-	else
-	{
-		echo "API key mismatch";
-	} 
+$conn=new mysqli($servername,$username,$password,$dbname); 
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 } 
-else
-{
-	echo "No POST data";
-} 
-
-function clean($data)
-{
-	$data=trim($data); 
-	$data=stripslashes($data); 
-	$data=htmlspecialchars($data); 
-	return $data;
+$sql="SELECT id,value1,value2,reading_time FROM Sensor order by reading_time desc limit 40"; 
+$sensor_data=array();
+$result=$conn->query($sql); 
+while ($row = $result->fetch_assoc()){
+  $sensor_data[] = $row;
 }
+$readings_time=array_column($sensor_data,'reading_time'); 
+$value1 = json_encode(array_reverse(array_column($sensor_data, 'value1')), JSON_NUMERIC_CHECK);
+$value2 = json_encode(array_reverse(array_column($sensor_data, 'value2')), JSON_NUMERIC_CHECK); 
+$reading_time = json_encode(array_reverse($readings_time), JSON_NUMERIC_CHECK); 
+$result->free(); 
+$conn->close();
+?> 
+
+<!DOCTYPE html>
+<html> 
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<style>
+body {
+  /*background-image: url('https://c4.wallpaperflare.com/wallpaper/282/308/59/abstract-vector-red-purple-wallpaper-preview.jpg'); */
+  min-width: 310px;
+    	max-width: 1280px;
+    	height: 500px;
+      margin: 0 auto;
+} 
+h2 {
+      font-family: Arial;
+      font-size: 2.5rem;
+      text-align: center;
+    }
+</style> 
+<body>
+<h2>Temperature and Pressure data</h2>
+    <div id="chart-temperature" class="container"></div>
+    <div id="chart-pressure" class="container"></div>
+	<script>
+var value1 = <?php echo $value1; ?>;
+var value2 = <?php echo $value2; ?>;
+var reading_time = <?php echo $reading_time; ?>;
+
+var chartT = new Highcharts.Chart({
+  chart:{ renderTo : 'chart-temperature' },
+  title: { text: 'BMP280 Temperature' },
+  series: [{
+    showInLegend: false,
+    data: value1
+  }],
+  plotOptions: {
+    line: { animation: false,
+      dataLabels: { enabled: true }
+    },
+    series: { color: '#059e8a' }
+  },
+  xAxis: { 
+    type: 'datetime',
+    categories: reading_time
+  },
+  yAxis: {
+    title: { text: 'Temperature (Celsius)' }
+    //title: { text: 'Temperature (Fahrenheit)' }
+  },
+  credits: { enabled: false }
+});
+
+var chartP = new Highcharts.Chart({
+  chart:{ renderTo:'chart-pressure' },
+  title: { text: 'BMP280 Pressure' },
+  series: [{
+    showInLegend: false,
+    data: value2
+  }],
+  plotOptions: {
+    line: { animation: false,
+      dataLabels: { enabled: true }
+    },
+    series: { color: '#18009c' }
+  },
+  xAxis: {
+    type: 'datetime',
+    categories: reading_time
+  },
+  yAxis: {
+    title: { text: 'Pressure (hPa)' }
+  },
+  credits: { enabled: false }
+});
+
+</script>
+</body>
+</html>
