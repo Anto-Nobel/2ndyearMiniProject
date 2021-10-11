05632d69173801c448f5a1e6d2ab1f4621682be9
Anto-Nobel <nobel2002@gmail.com>
2021-10-11 21:34:40 +0530

    final v0.0.1c

--- uno/uno.ino
+++ uno/uno.ino
@@ -1,13 +1,19 @@
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT); 
 }
 
 void loop() {
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
   int y1 = digitalRead(6);
-  int y12= digitalRead();
-  int y13= digitalRead();
+  int y12= digitalRead(5);
+  int y13= digitalRead(3);
   Serial.println(y1); 
   digitalWrite(9,y1);
-  
+  digitalWrite(10,y12);
+  digitalWrite(11,y13);
   delay(100);
 }
